e6ad4f9f9d780e20eae9a4e2cbb2529c4eca3d49
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-03-23 12:36:00 +0800

    js: 修改監聽器邏輯. operatorTransfer()及calculation()原則不變

--- src/index.js
+++ src/index.js
@@ -1,113 +1,82 @@
 const numberPad = document.querySelector('.numberPad')
-const numberBtn = document.querySelector('.number')
-const operatorBtn = document.querySelector('.operator')
-const backspaceBtn = document.querySelector('.backspace')
-const deleteBtn = document.querySelector('.delete')
-const getResultBtn = document.querySelector('.getResult')
-const calculationInput = document.querySelector('.calculationInput')
-const calculationResult = document.querySelector('.calculationResult')
+const input = document.querySelector('.calculationInput')
+const result = document.querySelector('.calculationResult')
+const BASE_URL = 'http://localhost:3000/'
 
-//運算式包含三個值[ 數字1, 加減乘除, 數字2]
-let inputList = []
-
-//監聽整個鍵盤，紀錄按下按鈕的值，當湊滿運算式三個值，便執行算數
-numberPad.addEventListener('click', async function numberPadClicked(event) {
-  //按下按鈕的值
-  let itemClicked = event.target.textContent.trim()
-  console.log(itemClicked)
-
-  //紀錄運算式第一個值(數字1)
-  if (inputList.length === 0) {
-    console.log('if判定: length is 0')
-    if (!isNaN(itemClicked)) {
-      console.log('是數字:', itemClicked)
-      inputList[0] = itemClicked
-      console.log('數字1_更新之inputList:', inputList)
-      calculationInput.textContent = inputList[0]
-    }
-  }
-  //紀錄運算式第一個值(數字1)及第二個值(加減乘除)
-  else if (inputList.length === 1) {
-    console.log('if判定: length is 1')
-    if (!isNaN(itemClicked)) {
-      console.log('是數字:', itemClicked)
-      inputList[0] = inputList[0] + itemClicked
-      console.log('數字1_更新之inputList:', inputList)
-      calculationInput.textContent = inputList[0]
-    } else if (
-      itemClicked === '+' ||
-      itemClicked === '-' ||
-      itemClicked === '*' ||
-      itemClicked === '/'
-    ) {
-      console.log('是運算子:', itemClicked)
-      inputList[1] = itemClicked
-      console.log('數字1+/_更新之inputList:', inputList)
-      calculationInput.textContent = `${inputList[0]} ${inputList[1]}`
-    }
-  }
-  //紀錄運算式第二個值(加減乘除)及第三個值(數字2)
-  else if (inputList.length === 2) {
-    console.log('if判定: length is 2')
-    if (!isNaN(itemClicked)) {
-      console.log('是數字:', itemClicked)
-      inputList[2] = itemClicked
-      console.log('更新之inputList:', inputList)
-      calculationInput.textContent = `${inputList[0]} ${inputList[1]} ${inputList[2]}`
-      const result = await calculation() //●
-      calculationResult.textContent = result //●
-    } else if (
-      itemClicked === '+' ||
-      itemClicked === '-' ||
-      itemClicked === '*' ||
-      itemClicked === '/'
-    ) {
-      console.log('是運算子:', itemClicked)
-      inputList[1] = itemClicked
-      console.log('數字1+/更新之inputList:', inputList)
-      calculationInput.textContent = `${inputList[0]} ${inputList[1]}`
-    }
-  }
-  //紀錄運算式第三個值(數字2)、更新第二個值(加減乘除)、直接算出結果
-  else if (inputList.length === 3) {
-    console.log('if判定: length is 3')
-    if (!isNaN(itemClicked)) {
-      console.log('是數字:', itemClicked)
-      inputList[2] = inputList[2] + itemClicked
-      console.log('更新之inputList:', inputList)
-      calculationInput.textContent = `${inputList[0]} ${inputList[1]} ${inputList[2]}`
-      const result = await calculation() //●
-      calculationResult.textContent = result //●
-    } else if (
-      itemClicked === '+' ||
-      itemClicked === '-' ||
-      itemClicked === '*' ||
-      itemClicked === '/'
-    ) {
-      console.log('是運算子:', itemClicked)
-      const result = await calculation() //●
-      inputList[0] = result //●
-      inputList[1] = itemClicked
-      inputList.pop()
-      console.log('更新之inputList:', inputList)
-      calculationInput.textContent = `${inputList[0]} ${inputList[1]}`
-      calculationResult.textContent = ''
-    } else if (itemClicked === '=') {
-      console.log('是送出結果:', itemClicked)
-      const result = await calculation() //●
-      inputList[0] = result
-      inputList.pop()
-      inputList.pop()
-      console.log('= 更新之inputList:', inputList)
-      calculationInput.textContent = `${inputList[0]}`
-      calculationResult.textContent = ''
+numberPad.addEventListener('click', async (event) => {
+  const target = event.target
+  const key = target.innerText
+  // 排除empty鍵及間隙
+  if (target.classList.contains('button')) {
+    // AC (All Clear)
+    if (target.classList.contains('delete')) {
+      // input歸零
+      input.innerText = '0'
+    } 
+    // 退格鍵
+    else if (target.classList.contains('backspace')) {
+      if (input.innerText.length > 1) {
+        // 刪除最後一個值
+        input.innerText = input.innerText.slice(0, -1)
+      } else {
+        // 個位數即歸零
+        input.innerText = '0'
+      }
+    } 
+    // 等於鍵
+    else if (target.classList.contains('equal')) {
+      if (!isNaN(input.innerText)) {
+        // 如input沒有運算子,input直接進入result
+        result.innerText = input.innerText
+      } else {
+        // 如input有運算子:
+        let operator = ''
+        // (1)找到運算子並宣告至operator變數
+        for (let i = 0; i < input.innerText.length; i++) {
+          if (isNaN(input.innerText[i])) {
+            operator = input.innerText[i]
+            break
+          }
+        }
+        // 使用split將operator左右數值變成陣列
+        const inputList = input.innerText.split(operator)
+        // operator轉換成request path推至inputList[供calculation()計算用]
+        inputList.push(operatorTransfer(operator))
+        // result顯示答案
+        result.innerText = await calculation(inputList)
+      }
+      // input歸零
+      input.innerText = 0
+    } 
+    // 數字 & 運算子
+    else {
+      if (input.innerText === '0') {
+        if (!isNaN(key)) {
+          // 數字取代0
+          input.innerText = key
+        } else if (isNaN(key)) {
+          // 沿用result值繼續計算
+          input.innerText = result.innerText + key
+        }
+      } else if (!isNaN(input.innerText.slice(-1))) {
+        // 數字可以疊加: 數字 & 運算子
+        input.innerText += key
+      } else if (isNaN(input.innerText.slice(-1))) {
+        if (!isNaN(key)) {
+          // 運算子可以疊加: 數字
+          input.innerText += key
+        } else if (isNaN(key)) {
+          // 運算子不可以疊加: 運算子
+          input.innerText = input.innerText.slice(0, -1) + key
+        }
+      }
     }
   }
 })
 
-// 計算前轉換運算子為字串
-function operatorTransfer() {
-  switch (inputList[1]) {
+// operator => request path
+function operatorTransfer(operator) {
+  switch (operator) {
     case '+':
       return 'plus'
       break
@@ -125,17 +94,15 @@ function operatorTransfer() {
   }
 }
 
-async function calculation() {
+async function calculation(inputList) {
   try {
     console.log('do calculation')
     let v1 = Number(inputList[0])
-    let operator = operatorTransfer()
-    let v2 = Number(inputList[2])
-    console.log(`http://localhost:5000/${operator}?v1=${v1}&v2=${v2}`)
+    let operator = inputList[2]
+    let v2 = Number(inputList[1])
+    console.log(`${BASE_URL}${operator}?v1=${v1}&v2=${v2}`)
 
-    const response = await axios.get(
-      `http://localhost:5000/${operator}?v1=${v1}&v2=${v2}`
-    )
+    const response = await axios.get(`${BASE_URL}${operator}?v1=${v1}&v2=${v2}`)
     console.log(response.data.answer)
     return response.data.answer
   } catch (error) {
